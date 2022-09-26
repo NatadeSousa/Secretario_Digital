1c132d9c0e07447e0a9711b1a0525e9cc712284b
Natã de Sousa <natan345894@gmail.com>
2022-09-26 11:27:09 -0300

    Implementando interface da MainActivity

---
+++ app/src/main/java/com/example/secretario_digital/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.secretario_digital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import secretario_digital.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
