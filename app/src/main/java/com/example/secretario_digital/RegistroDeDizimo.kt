0271b4d193662126de940a9591899aa2bc7ef07e
Natã de Sousa de Paula <natan345894@gmail.com>
2022-09-26 16:01:23 -0300

    Mais modificaçõe realizadas na interface da MainActivity

---
+++ app/src/main/java/com/example/secretario_digital/RegistroDeDizimo.kt
@@ -0,0 +1,12 @@
+package com.example.secretario_digital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import secretario_digital.R
+
+class RegistroDeDizimo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_de_dizimo)
+    }
+}
