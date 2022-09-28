62ebbd704f64c835e53d67867d01479c7a4bfdd6
Natã de Sousa de Paula <natan345894@gmail.com>
2022-09-28 16:04:21 -0300

    Interface da Activity de registro de dízimo implementada

--- app/src/main/java/com/example/secretario_digital/MainActivity.kt
+++ app/src/main/java/com/example/secretario_digital/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.secretario_digital
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import secretario_digital.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main)
 
     }
